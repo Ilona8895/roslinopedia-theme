cb8acb01c15f5c2d78f642dc1e7bbab6296b5850
Ilona Melcher <ilona.melcher8@gmail.com>
2026-03-09 15:17:10 +0100

    update colors

--- footer.php
+++ footer.php
@@ -1,60 +1,62 @@
 <footer class="site-footer" id="contact">
-    <div class="container">
       <div class="site-footer__inner">
-        <div class="group">
-          <div class="site-footer__col-one">
-            <a href="<?php echo site_url() ?>" class="site-footer__logo">
-              <img src="<?php echo get_theme_file_uri('/images/logo.png') ?>" alt="Roslinopedia">
-            </a>
-            <h3 class="site-footer__heading">Kategorie roślin</h3>
-            <h4 class="site-footer__heading">Gleba</h4>
-            <nav class="nav-list">
-              <ul>
-                <?php wp_nav_menu(array(
-                  'theme_location' => 'footerMenuLocation',
-                )); ?>
-              </ul>
-            </nav>
-          </div>
-
-          <div class="site-footer__col-two-three-group">
-            <div class="site-footer__col-two">
-              <h3 class="site-footer__heading">Stanowisko</h3>
+        <div class="container">
+          <div class="group">
+            <div class="site-footer__col-one">
+              <a href="<?php echo site_url() ?>" class="site-footer__logo">
+                <img src="<?php echo get_theme_file_uri('/images/logo.png') ?>" alt="Roslinopedia">
+              </a>
+              <h3 class="site-footer__heading">Kategorie roślin</h3>
+              <h4 class="site-footer__heading">Gleba</h4>
               <nav class="nav-list">
                 <ul>
-                <?php wp_nav_menu(array(
-                  'theme_location' => 'footerMenuLocation2',
-                )); ?>
-                </ul>
-              </nav>
-              <h3 class="site-footer__heading">Typ rośliny</h3>
-              <nav class="nav-list">
-                <ul>
-                <?php wp_nav_menu(array(
-                  'theme_location' => 'footerMenuLocation3',
-                )); ?>
+                  <?php wp_nav_menu(array(
+                    'theme_location' => 'footerMenuLocation',
+                  )); ?>
                 </ul>
               </nav>
             </div>
-          </div>
 
-          <div class="site-footer__col-four">
-            <h3 class="site-footer__heading">Kontakt</h3>
-            <p class="site-footer__contact-item">
-              <strong>Ilona Melcher</strong>
-            </p>
-            <p class="site-footer__contact-item">
-              <i class="fas fa-phone"></i>
-              <a href="tel:+48728482184" class="site-footer__link">+48 728 482 184</a>
-            </p>
-            <p class="site-footer__contact-item">
-              <i class="fas fa-envelope"></i>
-              <a href="mailto:ilona.melcher8@gmail.com" class="site-footer__link">ilona.melcher8@gmail.com</a>
-            </p>
-            <p class="site-footer__contact-item">
-              <i class="fab fa-instagram"></i>
-              <a href="#" class="site-footer__link">Ilona8895</a>
-            </p>
+            <div class="site-footer__col-two-three-group">
+              <div class="site-footer__col-two">
+
+                <h4 class="site-footer__heading">Stanowisko</h4>
+                <nav class="nav-list">
+                  <ul>
+                  <?php wp_nav_menu(array(
+                    'theme_location' => 'footerMenuLocation2',
+                  )); ?>
+                  </ul>
+                </nav>
+                <h4 class="site-footer__heading">Typ rośliny</h4>
+                <nav class="nav-list">
+                  <ul>
+                  <?php wp_nav_menu(array(
+                    'theme_location' => 'footerMenuLocation3',
+                  )); ?>
+                  </ul>
+                </nav>
+              </div>
+            </div>
+
+            <div class="site-footer__col-four">
+              <h3 class="site-footer__heading">Kontakt</h3>
+              <p class="site-footer__contact-item">
+                <strong>Ilona Melcher</strong>
+              </p>
+              <p class="site-footer__contact-item">
+                <i class="fas fa-phone"></i>
+                <a href="tel:+48728482184" class="site-footer__link">+48 728 482 184</a>
+              </p>
+              <p class="site-footer__contact-item">
+                <i class="fas fa-envelope"></i>
+                <a href="mailto:ilona.melcher8@gmail.com" class="site-footer__link">ilona.melcher8@gmail.com</a>
+              </p>
+              <p class="site-footer__contact-item">
+                <i class="fab fa-github"></i>
+                <a href="https://github.com/Ilona8895" target="_blank" class="site-footer__link">Ilona8895</a>
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -66,7 +68,7 @@
           <p>Projekt i wykonanie: Ilona Melcher</p>
         </div>
       </div>
-    </div>
+
 
 </footer>
 
