93329358d8ad3280815ae2f8a00d95decb6be0a7
Ilona Melcher <ilona.melcher8@gmail.com>
2026-03-18 14:08:28 +0100

    creating project page

--- single-roslina.php
+++ single-roslina.php
@@ -1,14 +1,13 @@
 <?php get_header(); ?>
 
-<?php
-  while(have_posts()) {
-    the_post(); 
-    
+<?php while (have_posts()) {
 
-    pageBanner(array(
-      'photo' => get_the_post_thumbnail_url(get_the_ID(), 'bannerImage')
-    ));
-    ?>
+  the_post();
+
+  pageBanner([
+    'photo' => get_the_post_thumbnail_url(get_the_ID(), 'bannerImage'),
+  ]);
+  ?>
 
 
   <section class="plant-single">
@@ -22,19 +21,29 @@
         <div class="plant-single__icon-wrapper">
 
         <?php
-          $terms = get_the_terms(get_the_ID(), 'typ');
-          $typ_icon = '';
-          if ($terms && !is_wp_error($terms)) {
-            $nazwa = $terms[0]->name;
-            switch ($nazwa) {
-              case 'Bylina':   $typ_icon = 'flower'; break;
-              case 'Drzewo':   $typ_icon = 'tree'; break;
-              case 'Krzew':    $typ_icon = 'bush'; break;
-              case 'Uprawne':  $typ_icon = 'vegetable'; break;
-            }
+        $terms = get_the_terms(get_the_ID(), 'typ');
+        $typ_icon = '';
+        if ($terms && !is_wp_error($terms)) {
+          $nazwa = $terms[0]->name;
+          switch ($nazwa) {
+            case 'Bylina':
+              $typ_icon = 'flower';
+              break;
+            case 'Drzewo':
+              $typ_icon = 'tree';
+              break;
+            case 'Krzew':
+              $typ_icon = 'bush';
+              break;
+            case 'Uprawne':
+              $typ_icon = 'vegetable';
+              break;
           }
+        }
         ?>
-        <img src="<?php echo get_theme_file_uri('/images/' . $typ_icon . '.png'); ?>" alt="ikona rośliny">
+        <img src="<?php echo get_theme_file_uri(
+          '/images/' . $typ_icon . '.png',
+        ); ?>" alt="ikona rośliny">
         </div>
       </div>
 
@@ -47,11 +56,11 @@
           </tr>
           <tr>
             <th>Stanowisko</th>
-            <td><?php echo get_the_term_list(get_the_ID(), 'stanowisko','',', '); ?></td>
+            <td><?php echo get_the_term_list(get_the_ID(), 'stanowisko', '', ', '); ?></td>
           </tr>
           <tr>
             <th>Gleba</th>
-            <td><?php echo get_the_term_list(get_the_ID(), 'gleba','',', '); ?></td>
+            <td><?php echo get_the_term_list(get_the_ID(), 'gleba', '', ', '); ?></td>
           </tr>
           <tr>
             <th>Wysokość (cm)</th>
@@ -68,45 +77,49 @@
         </table>
       </div>
 
-      <?php if(is_user_logged_in()) { ?>
+      <?php if (is_user_logged_in()) { ?>
       <div class="plant-single__notes">
-        <?php 
-        $notes = new WP_Query(array(
+        <?php
+        $notes = new WP_Query([
           'post_type' => 'notatka',
           'posts_per_page' => -1,
-          'meta_query' => array(
+          'meta_query' => [
             'relation' => 'OR',
-            array(
+            [
               'key' => 'powiazana_roslina',
               'value' => '"' . get_the_ID() . '"',
               'compare' => 'LIKE',
-            ),
-            array(
+            ],
+            [
               'key' => 'powiazana_roslina',
               'value' => get_the_ID(),
               'compare' => '=',
-            ),
-          ),
+            ],
+          ],
           'author' => get_current_user_id(),
-        ));
+        ]);
 
-   
-          while($notes->have_posts()) {
-            $notes->the_post();
-            ?>
+        while ($notes->have_posts()) {
+          $notes->the_post(); ?>
             <div class="note" note-id="<?php the_ID(); ?>">
               <div class="note__header">
-                <input readonly type="text" class="note__title" value="<?php echo str_replace('Prywatne: ', '', esc_attr(get_the_title())); ?>">
+                <input readonly type="text" class="note__title" value="<?php echo str_replace(
+                  'Prywatne: ',
+                  '',
+                  esc_attr(get_the_title()),
+                ); ?>">
                 <button class="btn btn--icon btn--primary note__edit-button"><i class="fas fa-edit"></i></button>
                 <button class="btn btn--icon btn--primary btn--hidden note__save-button"><i class="fas fa-save"></i></button>
                 <button class="btn btn--icon btn--danger note__delete-button" data-id="<?php the_ID(); ?>"><i class="fas fa-trash"></i></button>
               </div>
-              <textarea class="note__content" readonly><?php echo esc_textarea(wp_strip_all_tags(get_the_content())); ?></textarea>         
+              <textarea class="note__content" readonly><?php echo esc_textarea(
+                wp_strip_all_tags(get_the_content()),
+              ); ?></textarea>         
             </div>
             <?php
-          }
-          wp_reset_postdata();
-          ?>
+        }
+        wp_reset_postdata();
+        ?>
           <div class="new_note" plant-id="<?php the_ID(); ?>">
               <h3 class="new_note__header">Dodaj nową notatkę</h3>
               <input type="text" class="new_note__title" placeholder="Tytuł notatki" value="">
@@ -121,6 +134,7 @@
     
   </section>
 
-<?php } ?>
+<?php
+} ?>
 
 <?php get_footer(); ?>
